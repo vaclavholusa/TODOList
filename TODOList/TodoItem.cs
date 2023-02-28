8412b49c1b6685177feb56015028da6435c68f07
murinovam <murinovam@pojfm.cz>
2023-02-28 09:28:08 +0100

    Added TodoItem class

---
+++ TODOList/TodoItem.cs
@@ -0,0 +1,26 @@
+﻿namespace TODOList
+{
+    public enum Priority
+    {
+        Normal, // 0
+        High // 1
+    }
+
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public Priority Priority { get; set; }
+        public DateTime? Deadline { get; set; }
+        public DateTime DateAdded { get; set; }
+        public DateTime? DateFinished { get; set; }
+
+        public TodoItem(int id, string title, DateTime dateAdded)
+        {
+            Id = id;
+            Title = title;
+            DateAdded = dateAdded;
+        }
+    }
+}
