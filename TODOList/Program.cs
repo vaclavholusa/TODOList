5601042946f9c0423e407c8d45317874294cf55b
Václav Holuša <44590217+vaclavholusa@users.noreply.github.com>
2023-03-30 12:15:59 +0200

    Update Program.cs

--- TODOList/Program.cs
+++ TODOList/Program.cs
@@ -1,6 +1,46 @@
+using Todolist;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+var repo = new TodoItemsRepository();
+
+app.MapGet("/", () => repo.Items);
+app.MapGet("/seed", () => repo.Seed());
+app.MapPost("/new", (TodoItem item) => repo.AddNew(item));
+
+
+app.MapGet("/{id:int}", (int id) => 
+{
+    var item = repo.GetDetail(id);
+    if (item != null)
+    {
+        return Results.Ok(item);
+    }
+    else
+    {
+        return Results.NotFound();
+    }
+});
+
+app.MapDelete("/{id:int}", (int id) =>
+{
+    repo.DeleteItem(id);
+    return Results.NoContent();
+});
+
+app.MapPut("/{id:int}", (int id, TodoItem updatedItem) =>
+{
+    var updated = repo.UpdateItem(id, updatedItem);
+    if (updated != null)
+    {
+        return Results.Ok(updated);
+    }
+    else
+    {
+        return Results.NotFound();
+    }
+});
 
 app.Run();
