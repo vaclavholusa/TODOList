bd180ee0c44055403a3a9d60d232a708f29a59da
Václav Holuša <44590217+vaclavholusa@users.noreply.github.com>
2023-04-11 09:21:00 +0200

    Add files via upload

---
+++ TODOList/Migrations/20230411071052_AddedSubtitle.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Todolist.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedSubtitle : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Subtitle",
+                table: "Items",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Subtitle",
+                table: "Items");
+        }
+    }
+}
